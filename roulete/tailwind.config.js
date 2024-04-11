eec207cfdcff9eb8bbed6aff373b0e29e9a87cb1
Egor PermAdut <hdvhdvwww@gmail.com>
2024-04-11 10:39:39 +0300

    fix

---
+++ roulete/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./src/App.tsx", "./src/routes/Page404/Page404.tsx", "./src/routes/MainPage/MainPage.tsx"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
